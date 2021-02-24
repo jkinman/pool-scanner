ac03d6606420bd1e9eb9c65774259e328eee55b3
Joel Kinman <joel.kinman@appnovation.com>
2021-02-24 10:12:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const axios = require('axios')
 const moment = require('moment')
 
 const startDate = moment().subtract(0, 'day').toISOString()
-const endDate = moment().add(7, 'days').toISOString()
+const endDate = moment().add(2, 'days').toISOString()
 
 // const data = {'clubId': 3,'typeId':0,'resourceDetailId':149, 'sessionId':'91e696f7-79b0-4ebf-be54-6f24686014eb', 'startDate':'2021-02-24T00:00:00.000Z', 'endDate':'2021-02-25T00:00:00.000Z' }
 const data = {'clubId': 3,'typeId':0,'resourceDetailId':149, 'sessionId':'91e696f7-79b0-4ebf-be54-6f24686014eb', startDate, endDate }
 
-console.log(moment().toISOString())
-// console.log(moment().add(8, 'days').toISOString())
+
+exports.handler = async (event, context) => {
 
 axios.post( 'https://ywcavancouver.mosoportal.com/FunctionalTemplates/Views/OnlineSchedulerFunctions.asmx/GetAvailability', data )
 .then(res => {
@@ -18,6 +18,11 @@ axios.post( 'https://ywcavancouver.mosoportal.com/FunctionalTemplates/Views/Onli
     openSlots.sort((a, b) => {
         return a.start > b.start ?  1 : -1
     } )
+    return {
+        statusCode: 200,
+        body: openSlots
+        }
+    
     openSlots.map(data => 
         {
             if(! data.title.includes('4/4')){
@@ -28,5 +33,15 @@ axios.post( 'https://ywcavancouver.mosoportal.com/FunctionalTemplates/Views/Onli
             )
   })
   .catch(error => {
-    console.error(error)
+      console.error(error)
+    return {
+        statusCode: 500,
+        body: error
+        }
   })
+
+  return {
+    statusCode: 200,
+    body: "no data available"
+    }
+}
