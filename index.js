188608e80b78a5e46b8997cebe4333dcd9148f9a
Joel Kinman <joel.kinman@appnovation.com>
2021-02-24 09:26:04 -0800

    first

---
+++ index.js
@@ -0,0 +1,32 @@
+const https = require('https');
+const axios = require('axios')
+const moment = require('moment')
+
+const startDate = moment().subtract(0, 'day').toISOString()
+const endDate = moment().add(7, 'days').toISOString()
+
+// const data = {'clubId': 3,'typeId':0,'resourceDetailId':149, 'sessionId':'91e696f7-79b0-4ebf-be54-6f24686014eb', 'startDate':'2021-02-24T00:00:00.000Z', 'endDate':'2021-02-25T00:00:00.000Z' }
+const data = {'clubId': 3,'typeId':0,'resourceDetailId':149, 'sessionId':'91e696f7-79b0-4ebf-be54-6f24686014eb', startDate, endDate }
+
+console.log(moment().toISOString())
+// console.log(moment().add(8, 'days').toISOString())
+
+axios.post( 'https://ywcavancouver.mosoportal.com/FunctionalTemplates/Views/OnlineSchedulerFunctions.asmx/GetAvailability', data )
+.then(res => {
+    console.log(`statusCode: ${res.statusCode}`)
+    const openSlots = res.data.d.filter( spot => ! spot.title.includes('4/4'))
+    openSlots.sort((a, b) => {
+        return a.start > b.start ?  1 : -1
+    } )
+    openSlots.map(data => 
+        {
+            if(! data.title.includes('4/4')){
+                console.log( {time:data.StartDateTime} )
+                // console.log( data )
+            }
+        }
+            )
+  })
+  .catch(error => {
+    console.error(error)
+  })
